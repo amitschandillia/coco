15265651b083a08f0ffb412f15b1693f3e11c92a
amitschandillia <amdroid666@gmail.com>
2018-09-05 21:13:27 +0000

    Extended built-in error page.

---
+++ pages/_error.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default class Error extends React.Component {
+  static getInitialProps({ res, err }) {
+    const statusCode = res ? res.statusCode : err ? err.statusCode : null;
+    return { statusCode }
+  }
+
+  render() {
+    return (
+      <p>
+        <h4>Damn!</h4>
+        {this.props.statusCode
+          ? `An error ${this.props.statusCode} occurred on server`
+          : 'An error occurred on client'}
+      </p>
+    )
+  }
+}
