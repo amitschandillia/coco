a6a888d77f6d5201a532345efc3ee40abeabe932
amitschandillia <amdroid666@gmail.com>
2018-09-16 14:10:22 +0000

    Cleaned up unwanted files and folders.

--- models/author.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const authorSchema = mongoose.Schema({
-  // _id: mongoose.Schema.Types.ObjectId,
-  name: String,
-  age: Number,
-});
-
-module.exports = mongoose.model('Author', authorSchema);
